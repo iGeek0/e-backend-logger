dafbba43d49fe03a53ee1e6519608140f9eb53e0
“Jesus <jesus.ochoa.rabelo@gmail.com>
2022-08-16 14:50:20 -0600

    adding security to the routes using json web token

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const validateToken = require('./services/validate-token');
 const connectToMongoDB = require('./services/db_connection');
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', require('./routes/logs.routes'));
-app.use('/api', require('./routes/main.routes'));
+app.use('/api', validateToken, require('./routes/logs.routes'));
+app.use('/api', validateToken, require('./routes/main.routes'));
 
 module.exports = app;
